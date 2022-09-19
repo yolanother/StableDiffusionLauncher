6f0ceb4d2a9f969933a09abb28b458b88ac59fbc
Aaron Jackson <yolanother@gmail.com>
2022-09-19 07:55:05 -0700

    Initial import

---
+++ StableDiffusionLauncher/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace StableDiffusionLauncher
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBoxConsole = new ConsoleControl.ConsoleControl();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // richTextBoxConsole
+            // 
+            this.richTextBoxConsole.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBoxConsole.IsInputEnabled = true;
+            this.richTextBoxConsole.Location = new System.Drawing.Point(-1, 1);
+            this.richTextBoxConsole.Name = "richTextBoxConsole";
+            this.richTextBoxConsole.SendKeyboardCommandsToProcess = false;
+            this.richTextBoxConsole.ShowDiagnostics = false;
+            this.richTextBoxConsole.Size = new System.Drawing.Size(802, 448);
+            this.richTextBoxConsole.TabIndex = 0;
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBoxConsole);
+            this.Name = "MainForm";
+            this.Text = "Stable Diffusion Service";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ConsoleControl.ConsoleControl richTextBoxConsole;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+    }
+}
+
---
+++ StableDiffusionLauncher/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StableDiffusionLauncher
+{
+    public partial class MainForm : Form
+    {
+        private Process currentProcess;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized) Hide();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Runs a process
+        /// </summary>
+        /// <param name="fileName">Name of the file.</param>
+        /// <param name="arguments">The arguments.</param>
+        public void StartProcess(string fileName, string arguments)
+        {
+            // Create the process start info
+            var processStartInfo = new ProcessStartInfo(fileName, arguments);
+
+            // Set the options
+            processStartInfo.UseShellExecute = false;
+            processStartInfo.ErrorDialog = false;
+            processStartInfo.CreateNoWindow = true;
+
+            // Specify redirection
+            processStartInfo.RedirectStandardError = true;
+            processStartInfo.RedirectStandardInput = true;
+            processStartInfo.RedirectStandardOutput = true;
+
+            // Create the process
+            currentProcess = new Process();
+            currentProcess.EnableRaisingEvents = true;
+            currentProcess.StartInfo = processStartInfo;
+            currentProcess.Exited += new EventHandler(currentProcess_Exited);
+
+            // Try to start the process
+            try
+            {
+                bool processStarted = currentProcess.Start();
+            }
+            catch (Exception e)
+            {
+                // We failed to start the process. Write the output (if we have diagnostics on).
+                if (ShowDiagnostics)
+                    WriteOutput("Failed: " + e.ToString() + Environment.NewLine, Color.Red);
+                return;
+            }
+
+            // Store name and arguments
+            currentProcessFileName = fileName;
+            currentProcessArguments = arguments;
+            // Create the readers and writers
+            inputWriter = currentProcess.StandardInput;
+            outputReader = TextReader.Synchronized(currentProcess.StandardOutput);
+            errorReader = TextReader.Synchronized(currentProcess.StandardError);
+
+            // Run the output and error workers
+            outputWorker.RunWorkerAsync();
+            errorWorker.RunWorkerAsync();
+
+            // If we enable input, make the control not read only
+            if (IsInputEnabled)
+                richTextBoxConsole.ReadOnly = false;
+        }
+}
